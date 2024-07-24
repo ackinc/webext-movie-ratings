c7ddfaa530495d6a42cd0e9c23fc2b0c0ed1cd7f
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2024-07-24 21:14:22 +0530

    src files now in src folder + content-script partially done

---
+++ src/common.js
@@ -0,0 +1,13 @@
+export function delayMs(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function waitFor(fn, maxTries = 10, intervalBetweenTriesMs = 500) {
+  let nTries = 0;
+  let val;
+  while (++nTries <= maxTries) {
+    if ((val = await fn())) return val;
+    await delayMs(intervalBetweenTriesMs);
+  }
+  throw new Error("waitFor timed out");
+}
