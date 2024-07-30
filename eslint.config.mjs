a5fffbd10e787c142a6e5776f399c68c4b60b4a0
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2024-07-30 11:50:58 +0530

    Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
+  { ignores: ["dist/*"] },
   {
+    files: ["src/**/*.js"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.webextensions },
+    },
+  },
+  {
+    files: ["scripts/**/*.js"],
+    languageOptions: {
+      globals: { ...globals.node },
     },
   },
   pluginJs.configs.recommended,
