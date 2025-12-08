207ee84ce10ebdd358cd29d333b93383cba2786a
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2025-12-08 16:00:30 +0530

    Fixed extension not working on AppleTV

--- src/AppleTV/Page.js
+++ src/AppleTV/Page.js
@@ -16,108 +16,34 @@ export default class AppleTvPage extends AbstractPage {
     styleNode.innerHTML = `
       a.${IMDB_DATA_NODE_CLASS} {
         color: var(--systemSecondary);
-      }
-
-      div.epic-inline-shelf a.${IMDB_DATA_NODE_CLASS} {
-        color: #9f9f9f;
-      }
-
-      a.ordinal-chart-lockup a.${IMDB_DATA_NODE_CLASS} {
-        font-size: 12px;
-      }
-
-      a.notes-lockup a.${IMDB_DATA_NODE_CLASS} {
-        display: block;
-        font-size: 12px;
-        margin-bottom: 2px;
-      }
-
-      a.search-card__link a.${IMDB_DATA_NODE_CLASS} {
-        font-size: 12px;
+        margin-left: 4px;
       }
     `;
   }
 
   findProgramContainerNodes() {
     // user is on MLS (sports) page
-    if (location.pathname.includes("/channel/")) return [];
+    if (location.pathname.includes("/channel/mls")) return [];
 
-    const selectors = [
-      // home page
-      "div.landing__main > div.shelf-grid",
-      "div.landing__main > div.epic-inline-shelf",
-      // page of particular movie/series
-      "div.product-main > div.shelf-grid",
-      // category pages
-      "div.canvas > div.shelf-grid",
-      "div.infinite-grid",
-      // top chart page
-      "div.collection-page.ordinal-chart",
-      // search results page
-      "div.canvas > div > div.shelf-grid:not(.epic-stage__content-shelf)",
-    ];
+    const selectors = ['div.section[aria-label]:not([aria-label=""])'];
     return Array.from(document.querySelectorAll(selectors.join(", ")));
   }
 
   getTitleFromProgramContainerNode(pContainerNode) {
-    if (
-      pContainerNode.matches("div.shelf-grid") ||
-      pContainerNode.matches("div.infinite-grid")
-    ) {
-      return pContainerNode
-        .querySelector("div.shelf-grid__header h2")
-        ?.textContent.trim();
-    }
-
-    if (pContainerNode.matches("div.epic-inline-shelf")) {
-      return pContainerNode
-        .querySelector("div.epic-inline-shelf__upsell-details__title")
-        .textContent.trim();
-    }
-
-    if (pContainerNode.matches("div.collection-page.ordinal-chart")) {
-      return pContainerNode.querySelector("h1").textContent.trim();
-    }
-
-    return null;
+    return pContainerNode.getAttribute("aria-label") ?? null;
   }
 
   isValidProgramContainer(pContainer) {
-    const { title } = pContainer;
-    return (
-      title &&
-      ![
-        // Home page
-        "Browse by Category",
-        // Particular movie/series page
-        /^Season \d+$/,
-        "Trailers",
-        "Bonus Content",
-        "Cast & Crew",
-        "How to Watch",
-      ].some((x) => (typeof x === "string" ? title === x : x.test(title)))
-    );
+    if (["/movie/", "/show/"].some((x) => location.pathname.includes(x)))
+      return pContainer.title === "Related";
+
+    return true;
   }
 
   findProgramsInProgramContainer(pContainer) {
-    let programNodes = [];
-
-    const { node } = pContainer;
-    if (
-      pContainer.node.matches("div.shelf-grid") ||
-      pContainer.node.matches("div.infinite-grid") ||
-      pContainer.node.matches("div.ordinal-chart")
-    ) {
-      programNodes = Array.from(
-        node.querySelectorAll(
-          "div.canvas-lockup[data-metrics-click],a.ordinal-chart-lockup[data-metrics-click],a.notes-lockup[data-metrics-click],a.search-card__link[data-metrics-click]"
-        )
-      );
-    } else if (pContainer.node.matches("div.epic-inline-shelf")) {
-      programNodes = Array.from(
-        node.querySelectorAll("div.shelf-grid div.canvas-lockup")
-      );
-    }
+    const programNodes = Array.from(
+      pContainer.node.querySelectorAll("ul > li a")
+    );
 
     const programs = programNodes
       .map((node) => ({
--- src/AppleTV/ProgramNode.js
+++ src/AppleTV/ProgramNode.js
@@ -6,70 +6,15 @@ export default class ProgramNode {
   }
 
   static extractData(programNode) {
-    const title =
-      programNode.getAttribute("aria-label") ||
-      JSON.parse(
-        programNode.dataset.metricsLocation ||
-          programNode.dataset.metricsImpressions
-      ).name;
-
-    const dataMetricsClick = JSON.parse(programNode.dataset.metricsClick);
-
-    const type =
-      dataMetricsClick.contentType === "Show"
-        ? "series"
-        : dataMetricsClick.contentType === "Movie"
-        ? "movie"
-        : null;
-
-    return { title, type };
+    const title = programNode.getAttribute("aria-label");
+    return { title, type: null };
   }
 
   static insertIMDBNode(programNode, imdbNode) {
-    if (programNode.matches("a.ordinal-chart-lockup")) {
-      programNode
-        .querySelector("div.ordinal-chart__metadata")
-        .appendChild(imdbNode);
-    } else if (programNode.matches("a.notes-lockup")) {
-      const x = programNode.querySelector("div.notes-lockup__right");
-      x.insertBefore(imdbNode, x.lastElementChild);
-    } else if (programNode.matches("a.search-card__link")) {
-      programNode
-        .querySelector("div.search-card__metadata")
-        .appendChild(imdbNode);
-    } else if (
-      programNode.querySelector(".lockup-overlay__promo-text-wrapper")
-    ) {
-      programNode.parentNode.appendChild(imdbNode);
-    } else if (programNode.querySelector("div.lockup-overlay")) {
-      programNode.parentNode.appendChild(imdbNode);
-    } else {
-      programNode.appendChild(imdbNode);
-    }
+    programNode.appendChild(imdbNode);
   }
 
   static getIMDBNode(programNode) {
-    const maybeImdbNode = programNode.matches("a.ordinal-chart-lockup")
-      ? programNode.querySelector("div.ordinal-chart__metadata")
-          .lastElementChild
-      : programNode.matches("a.notes-lockup")
-      ? programNode.querySelector("div.notes-lockup__right").lastElementChild
-          .previousElementSibling
-      : programNode.matches("a.search-card__link")
-      ? programNode.querySelector("div.search-card__metadata").lastElementChild
-      : programNode.querySelector(".lockup-overlay__promo-text-wrapper")
-      ? programNode.parentNode.lastElementChild
-      : programNode.querySelector("div.lockup-overlay")
-      ? programNode.parentNode.lastElementChild
-      : programNode.lastElementChild;
-
-    if (
-      maybeImdbNode &&
-      maybeImdbNode.classList.contains(IMDB_DATA_NODE_CLASS)
-    ) {
-      return maybeImdbNode;
-    } else {
-      return null;
-    }
+    return programNode.querySelector(`a.${IMDB_DATA_NODE_CLASS}`);
   }
 }
