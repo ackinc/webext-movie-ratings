71728387b47fbad04a0a2827d2bce6b58ba88fde
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2025-12-21 14:14:55 +0530

    Fixed type errors in modules in SonyLiv dir

--- src/SonyLiv/ProgramNode.ts
+++ src/SonyLiv/ProgramNode.ts
@@ -1,54 +1,55 @@
 import AbstractProgramNode from "../common/AbstractProgramNode";
 import { IMDB_DATA_NODE_CLASS, extractProgramTitle } from "../common";
+import type { Program } from "../common/types";
 
 export default class ProgramNode extends AbstractProgramNode {
-  static isMovieOrSeries(node) {
+  static override isMovieOrSeries(node: HTMLElement): boolean {
     if (node.matches("a.trending-tray-link")) {
       // we can't say for sure, so always return true
       return true;
     }
 
-    const href = node.getAttribute("href");
+    const href = node.getAttribute("href") ?? "";
     return ["/movies", "/shows", "/trailer"].some((x) => href.startsWith(x));
   }
 
-  static extractData(node) {
-    const data = {};
-
-    data.title = extractProgramTitle(
+  static override extractData(node: HTMLElement): Omit<Program, "node"> {
+    const title = extractProgramTitle(
       node.getAttribute("title") ||
-        node.querySelector("div.album-cover-container > img[title]")?.title ||
+        node
+          .querySelector("div.album-cover-container > img[title]")
+          ?.getAttribute("title") ||
         ""
     );
 
     const href = node.getAttribute("href");
-    data.type = href.startsWith("/movies")
+    const type = href?.startsWith("/movies")
       ? "movie"
-      : href.startsWith("/shows")
+      : href?.startsWith("/shows")
         ? "series"
         : undefined;
 
-    return data;
+    return { title, ...(type ? { type } : {}) };
   }
 
-  static insertIMDBNode(node, imdbNode) {
-    if (node.parentNode.classList.contains("innerlistt")) {
-      node.firstChild.style.marginBottom = "2em";
-      node.firstChild.firstChild.appendChild(imdbNode);
+  static override insertIMDBNode(node: HTMLElement, imdbNode: HTMLElement) {
+    if ((node.parentNode as HTMLElement).classList.contains("innerlistt")) {
+      (node.firstChild as HTMLElement).style.marginBottom = "2em";
+      node.firstChild!.firstChild!.appendChild(imdbNode);
     } else {
-      node.parentNode.parentNode.appendChild(imdbNode);
+      node.parentNode!.parentNode!.appendChild(imdbNode);
     }
   }
 
-  static getIMDBNode(node) {
-    if (node.parentNode.classList.contains("innerlistt")) {
-      const candidate = node.firstChild.firstChild.lastChild;
+  static override getIMDBNode(node: HTMLElement): HTMLElement | null {
+    if ((node.parentNode as HTMLElement).classList.contains("innerlistt")) {
+      const candidate = node.firstChild!.firstChild!.lastChild! as HTMLElement;
       return candidate.classList.contains(IMDB_DATA_NODE_CLASS)
         ? candidate
         : null;
     }
 
-    const candidate = node.parentNode.parentNode.lastChild;
+    const candidate = node.parentNode!.parentNode!.lastChild! as HTMLElement;
     return candidate.classList.contains(IMDB_DATA_NODE_CLASS)
       ? candidate
       : null;
