dfc2d4b01161578c143e26d6c3ae0d3eb6d2f430
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-01-13 15:55:41 +0530

    Popup now explains what data is collected when user opts in to error reporting

--- src/popup/main.jsx
+++ src/popup/main.jsx
@@ -22,22 +22,54 @@ function App() {
   return (
     <div
       style={{
-        width: "200px",
+        width: "320px",
         padding: "16px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
         fontSize: "0.875rem",
       }}
     >
-      <label for="optInToErrorReporting">Opt-in to error reporting?</label>
-      <input
-        type="checkbox"
-        id="optInToErrorReporting"
-        name="optIn"
-        checked={optedInToErrorReporting}
-        onChange={toggleErrorReportingOptIn}
-      />
+      <div
+        style={{
+          marginBottom: "1rem",
+          borderRadius: "2rem",
+          padding: "1rem",
+          backgroundColor: "#e1bf2b",
+          color: "#454545",
+          fontSize: "0.75rem",
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.5rem",
+        }}
+      >
+        <p style={{ fontWeight: "bold" }}>ⓘ Opt-in to error reporting?</p>
+        <p>
+          With your permission, we can collect the following information when an
+          error occurs in the extension.
+        </p>
+        <ul style={{ paddingLeft: "1rem" }}>
+          <li>Device, OS, and browser</li>
+          <li>Dimensions of the browser</li>
+          <li>Error details</li>
+        </ul>
+        <p>This will help us deliver fixes faster.</p>
+      </div>
+
+      <div
+        style={{
+          padding: "0 1rem",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <label for="optInToErrorReporting">Opt-in to error reporting?</label>
+        <input
+          type="checkbox"
+          id="optInToErrorReporting"
+          name="optIn"
+          checked={optedInToErrorReporting}
+          onChange={toggleErrorReportingOptIn}
+        />
+      </div>
     </div>
   );
 
