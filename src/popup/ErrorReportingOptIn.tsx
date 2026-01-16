a003fd5fceb8dbfd0b99f5c92cded651cdb5ed0b
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-01-16 14:52:04 +0530

    Improved styles for error-reporting-opt-in control + moved styles to css file

--- src/popup/ErrorReportingOptIn.tsx
+++ src/popup/ErrorReportingOptIn.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "preact/hooks";
 import { getSetting, setSetting, SettingsKey } from "../common";
+import "./ErrorReportingOptIn.css";
 
 function ErrorReportingOptIn() {
+  const [showDetails, setShowDetails] = useState(false);
   const [optedInToErrorReporting, setOptedInToErrorReporting] = useState(false);
 
   useEffect(() => {
@@ -14,47 +16,8 @@ function ErrorReportingOptIn() {
   }, []);
 
   return (
-    <div
-      className="error-reporting-opt-in-container"
-      style={{ marginBottom: "1rem" }}
-    >
-      <div
-        style={{
-          marginBottom: "1rem",
-          borderRadius: "2rem",
-          padding: "1rem",
-          backgroundColor: "#f5c618",
-          color: "#454545",
-          fontSize: "0.75rem",
-          display: "flex",
-          flexDirection: "column",
-          gap: "0.5rem",
-        }}
-      >
-        <p style={{ fontWeight: "bold" }}>ⓘ Opt-in to error reporting?</p>
-        <p>
-          With your permission, we can collect the following information when an
-          error occurs in the extension.
-        </p>
-        <ul style={{ paddingLeft: "1rem" }}>
-          <li>Device, OS, and browser</li>
-          <li>Dimensions of the browser</li>
-          <li>Error details</li>
-        </ul>
-        <p>
-          This will help us deliver fixes faster when updates to an OTT
-          website's design causes the extension to break.
-        </p>
-      </div>
-
-      <div
-        style={{
-          padding: "0 1rem",
-          display: "flex",
-          alignItems: "center",
-          gap: "0.25rem",
-        }}
-      >
+    <div className="error-reporting-container">
+      <div className="error-reporting-control">
         <input
           type="checkbox"
           id="optInToErrorReporting"
@@ -62,8 +25,32 @@ function ErrorReportingOptIn() {
           checked={optedInToErrorReporting}
           onChange={toggleErrorReportingOptIn}
         />
-        <label for="optInToErrorReporting">Opt-in to error reporting?</label>
+        <label for="optInToErrorReporting">Opt-in to error reporting</label>
+        <button
+          className="toggle-info-btn"
+          onClick={() => setShowDetails((x) => !x)}
+        >
+          ?
+        </button>
       </div>
+
+      {showDetails ? (
+        <div className="error-reporting-details">
+          <p>
+            With your permission, we can collect the following information when
+            an error occurs in the extension.
+          </p>
+          <ul>
+            <li>Device, OS, and browser</li>
+            <li>Dimensions of the browser</li>
+            <li>Error details</li>
+          </ul>
+          <p>
+            This will help us deliver fixes faster when updates to an OTT
+            website's design causes the extension to break.
+          </p>
+        </div>
+      ) : null}
     </div>
   );
 
