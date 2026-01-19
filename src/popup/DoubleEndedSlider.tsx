c3dc74d24ab78660d066395e0f23c2b9aa13e007
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-01-19 14:23:34 +0530

    Bugfix: transparency slider was resetting to original value whenever rating slider is updated
    Originally thought this bug was fixed in 382f343, but was mistaken
    
    See https://github.com/ackinc/webext-movie-ratings/issues/3

--- src/popup/DoubleEndedSlider.tsx
+++ src/popup/DoubleEndedSlider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "preact/hooks";
+import { useEffect, useState, useRef } from "preact/hooks";
 import {
   create as createSlider,
   type target as TargetElement,
@@ -8,33 +8,45 @@ import "nouislider/dist/nouislider.css";
 
 import type { NumberRange } from "../common";
 
-export default function DoubleEndedSlider(props: DoubleEndedSliderProps) {
+export default function DoubleEndedSlider({
+  className,
+  defaultValues,
+  range,
+  step,
+  onInput,
+}: DoubleEndedSliderProps) {
+  const [sliderCreated, setSliderCreated] = useState(false);
   const sliderRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const slider = sliderRef.current!;
-
     createSlider(slider, {
-      start: [props.defaultValues.min, props.defaultValues.max],
-      range: props.range,
-      step: props.step,
+      start: [defaultValues.min, defaultValues.max],
+      range,
+      step,
       connect: true,
       behaviour: "tap-drag",
     });
+    setSliderCreated(true);
+  }, []);
 
+  // update the nouislider's 'update' event listener whenever the onInput prop
+  //   changes; fixes https://github.com/ackinc/webext-movie-ratings/issues/3
+  useEffect(() => {
+    if (!sliderCreated) return;
+
+    const slider = sliderRef.current! as TargetElement;
+    slider.noUiSlider!.off("update");
     // WARNING: nouislider triggers the 'update' event as many times during
     //   initial setup of this component as there are handles on the slider,
     //   even though these are not 'true updates'
-    (slider as TargetElement).noUiSlider!.on("update", (values) => {
-      props.onInput({ min: Number(values[0]), max: Number(values[1]) });
+    slider.noUiSlider!.on("update", (values) => {
+      onInput({ min: Number(values[0]), max: Number(values[1]) });
     });
-  }, []);
+  }, [sliderCreated, onInput]);
 
   return (
-    <div
-      ref={sliderRef}
-      className={`double-ended-slider ${props.className ?? ""}`}
-    />
+    <div ref={sliderRef} className={`double-ended-slider ${className ?? ""}`} />
   );
 }
 
