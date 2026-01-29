115cab991d9b05fd6618789524276f275159643c
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-01-29 13:41:03 +0530

    Migrated bumpVersion script to ts

--- scripts/bumpVersion.ts
+++ scripts/bumpVersion.ts
@@ -1,29 +1,28 @@
-const { execSync } = require("node:child_process");
-const fs = require("node:fs");
-const path = require("node:path");
+import { execSync } from "node:child_process";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
-const allowedVersionBumpTypes = ["major", "minor", "patch"];
+type VersionBumpType = "major" | "minor" | "patch";
 const versionBumpType = process.argv[2]?.replace(/^--/, "") ?? "patch";
 const writeFiles = !process.argv.includes("--nowrite");
 
-if (!allowedVersionBumpTypes.includes(versionBumpType)) {
+if (!isAllowedVersionBumpType(versionBumpType)) {
   console.error(
     `Error. Usage: node bumpVersion.cjs [TYPE] [--nowrite]
-    TYPE: one of "--major", "--minor", "--patch" (default)`
+    TYPE: one of "--major", "--minor", "--patch" (default)`,
   );
   process.exit(1);
 }
 
 const projectRoot = path.join(__dirname, "..");
 const fileList = ["package.json", "manifest.json"].map((f) =>
-  path.join(projectRoot, f)
+  path.join(projectRoot, f),
 );
 
-let oldVersion;
-let newVersion;
-
-fileList.forEach((f) => {
-  const contents = require(f);
+let oldVersion: string;
+let newVersion: string;
+for (const f of fileList) {
+  const contents = await import(f);
   oldVersion ??= contents.version;
   newVersion ??= bumpVersion(contents.version, versionBumpType);
   contents.version = newVersion;
@@ -35,9 +34,9 @@ fileList.forEach((f) => {
     //   when manifest files are manually edited later
     execSync(`npx prettier ${f} --write`, { stdio: "pipe" });
   }
-});
+}
 
-const logMsg = `Bumped version: v${oldVersion} to v${newVersion}`;
+const logMsg = `Bumped version: v${oldVersion!} to v${newVersion!}`;
 
 if (writeFiles) {
   try {
@@ -45,11 +44,11 @@ if (writeFiles) {
       `git add ${fileList
         .map((x) => `"${x}"`)
         .join(" ")} && git commit -m "${logMsg}"`,
-      { stdio: "pipe" }
+      { stdio: "pipe" },
     );
-    execSync(`git tag v${newVersion}`, { stdio: "pipe" });
+    execSync(`git tag v${newVersion!}`, { stdio: "pipe" });
   } catch (e) {
-    console.error(`Error: ${e.message}`);
+    console.error(`Error: ${(e as Error).message}`);
     process.exit(1);
   }
 }
@@ -58,8 +57,12 @@ console.log(logMsg);
 
 // HELPERS
 
-function bumpVersion(oldVersion, versionBumpType) {
-  let [major, minor, patch] = oldVersion.split(".").map((x) => +x);
+function bumpVersion(oldVersion: string, versionBumpType: VersionBumpType) {
+  let [major, minor, patch] = oldVersion.split(".").map((x) => +x) as [
+    number,
+    number,
+    number,
+  ];
 
   switch (versionBumpType) {
     case "major":
@@ -79,4 +82,8 @@ function bumpVersion(oldVersion, versionBumpType) {
   }
 
   return [major, minor, patch].join(".");
+}
+
+function isAllowedVersionBumpType(s: string): s is VersionBumpType {
+  return s in ["major", "minor", "patch"];
 }
