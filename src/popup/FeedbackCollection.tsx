3c4d2ba661b3c05b689c775b42f769916319f275
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-02-12 23:36:22 +0530

    Better collapse toggle for FeedbackCollection component

--- src/popup/FeedbackCollection.tsx
+++ src/popup/FeedbackCollection.tsx
@@ -1,35 +1,33 @@
 import { useState } from "preact/hooks";
 import CopyToClipboardButton from "./CopyToClipboardButton";
-import CloseButton from "./CloseButton";
 import "./FeedbackCollection.css";
 
 const email = "anirudh.nimmagadda@gmail.com";
 
 export default function FeedbackCollection() {
-  const [expanded, setExpanded] = useState(true);
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <div className={`feedback-collection ${expanded ? "expanded" : ""}`}>
-      {!expanded ? (
-        <button className="btn-expand" onClick={() => setExpanded(true)}>
-          Have feedback?
-        </button>
-      ) : (
-        <>
-          <CloseButton onClick={() => setExpanded(false)} />
+      <button
+        className="btn-toggle-expand"
+        onClick={() => setExpanded((x) => !x)}
+      >
+        {expanded ? "Close" : "Have feedback?"}
+      </button>
 
-          <div className="instructions">
-            <p>
-              Please send an email to{" "}
-              <span className="email">
-                {email} <CopyToClipboardButton textToCopy={email} />
-              </span>{" "}
-            </p>
+      {expanded ? (
+        <div className="instructions">
+          <p>
+            Please send an email to{" "}
+            <span className="email">
+              {email} <CopyToClipboardButton textToCopy={email} />
+            </span>{" "}
+          </p>
 
-            <p>Thanks for helping make Sift better!</p>
-          </div>
-        </>
-      )}
+          <p>Thank you for helping make Sift better!</p>
+        </div>
+      ) : null}
     </div>
   );
 }
