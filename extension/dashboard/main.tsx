33525a32165d651e03e55906893a6729637b01c8
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-03-12 19:58:25 +0530

    Dashboard now has access to telemetryStore

--- extension/dashboard/main.tsx
+++ extension/dashboard/main.tsx
@@ -2,11 +2,21 @@
 //   though they are unused
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { render, h, Fragment } from "preact";
+import { type IDBPDatabase } from "idb";
+import { upgradeIdbAndGetConnection } from "../common";
+import TelemetryStore, {
+  type TelemetryStoreSchema,
+} from "../common/TelemetryStore";
+import Dashboard from "./Dashboard";
 import "./main.css";
 
-const root = document.querySelector<HTMLDivElement>("div#root")!;
-render(<App />, root);
+(async () => {
+  const db = await upgradeIdbAndGetConnection();
+  const telemetryStore = await TelemetryStore.create(
+    db as IDBPDatabase<TelemetryStoreSchema>,
+    1, // doesn't matter - we're read-only
+  );
 
-function App() {
-  return <div className="app">Placeholder</div>;
-}
+  const root = document.querySelector<HTMLDivElement>("div#root")!;
+  render(<Dashboard telemetryStore={telemetryStore} />, root);
+})();
