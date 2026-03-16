64c81d713fa563003f896f6a890c713992ca7806
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-03-16 15:06:10 +0530

    SitePermsControlForm now initializes checkboxes depending on what perms have already been given

--- extension/popup/SitePermsControlForm.tsx
+++ extension/popup/SitePermsControlForm.tsx
@@ -1,44 +1,89 @@
 import { useEffect, useState } from "preact/hooks";
+import { browser } from "../common";
 import "./SitePermsControlForm.css";
 
-const supportedSites = [
-  "amazonprimevideo",
-  "appletv",
-  "crunchyroll",
-  "hotstar",
-  "netflix",
-  "sonyliv",
-  "youtubemovies",
-] as const;
-type Sitename = (typeof supportedSites)[number];
+const supportedSites = {
+  amazonprimevideo: {
+    displayName: "Amazon Prime Video",
+    permStrings: ["https://www.primevideo.com/*", "https://www.amazon.com/*"],
+  },
+  appletv: {
+    displayName: "AppleTV",
+    permStrings: ["https://tv.apple.com/*"],
+  },
+  crunchyroll: {
+    displayName: "Crunchyroll",
+    permStrings: ["https://www.crunchyroll.com/*"],
+  },
+  hotstar: {
+    displayName: "Hotstar",
+    permStrings: ["https://www.hotstar.com/*"],
+  },
+  netflix: {
+    displayName: "Netflix",
+    permStrings: ["https://www.netflix.com/*"],
+  },
+  sonyliv: {
+    displayName: "SonyLIV",
+    permStrings: ["https://www.sonyliv.com/*"],
+  },
+  youtubemovies: {
+    displayName: "Youtube Movies",
+    permStrings: ["https://www.youtube.com/*"],
+  },
+} as const;
+type Sitename = keyof typeof supportedSites;
+type PermString = (typeof supportedSites)[Sitename]["permStrings"][number];
 type IsEnabled = boolean;
+
+const permStringToSitename = Object.entries(supportedSites).reduce(
+  (acc, [sitename, { permStrings }]) =>
+    Object.assign(
+      acc,
+      permStrings.reduce(
+        (acc2, ps) => Object.assign(acc2, { [ps]: sitename }),
+        {},
+      ),
+    ),
+  {},
+) as Record<PermString, Sitename>;
 
 export default function SitePermsControlForm() {
   const [sitePerms, setSitePerms] = useState(
-    supportedSites.reduce(
+    Object.keys(supportedSites).reduce(
       (acc, site) => Object.assign(acc, { [site]: false }),
       {} as Record<Sitename, IsEnabled>,
     ),
   );
 
-  // TODO: load persisted settings
-  useEffect(() => {}, []);
+  useEffect(() => {
+    (async () => {
+      const allOrigins = (await browser.permissions.getAll()).origins ?? [];
+      const persistedPerms = allOrigins.reduce(
+        (acc, o) =>
+          Object.assign(acc, { [permStringToSitename[o as PermString]]: true }),
+        {},
+      );
+      setSitePerms((old) => ({ ...old, ...persistedPerms }));
+    })();
+  }, []);
 
   return (
     <form className="site-control-form">
       <h4>What OTT sites should Sift run on?</h4>
 
-      {supportedSites.map((site) => {
+      {(Object.keys(supportedSites) as Sitename[]).map((site) => {
+        const label = `sitePermFor${supportedSites[site].displayName.replace(/\s/g, "")}`;
         return (
           <div key={site} className="form-control">
             <input
               type="checkbox"
-              id={`sitePermFor_${site}`}
-              name={`sitePermFor_${site}`}
+              id={label}
+              name={label}
               checked={sitePerms[site]}
               onChange={() => toggleSitePerms(site)}
             />
-            <label for={`sitePermFor_${site}`}>{site}</label>
+            <label for={label}>{supportedSites[site].displayName}</label>
           </div>
         );
       })}
