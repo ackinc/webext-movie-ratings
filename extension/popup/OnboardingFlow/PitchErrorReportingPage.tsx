6c06efc8f5fbe1ee4e7d82c806ad32da289fba6e
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-03-20 16:09:49 +0530

    Completed PitchErrorReportingPage in onboarding flow

--- extension/popup/OnboardingFlow/PitchErrorReportingPage.tsx
+++ extension/popup/OnboardingFlow/PitchErrorReportingPage.tsx
@@ -1,3 +1,45 @@
+import ErrorReportingOptIn from "../ErrorReportingOptIn";
+import "./PitchErrorReportingPage.css";
+
 export default function PitchErrorReportingOptInPage() {
-  return <div className="onboarding-pitch-error-reporting-page"></div>;
+  return (
+    <div className="page pitch-error-reporting-page">
+      <p>
+        This extension collects <b>NO</b> data by default ...
+      </p>
+
+      <p>
+        ... but you can opt-in to automatic error reporting below to notify the
+        developer when the extension encounters an error.
+      </p>
+
+      <ErrorReportingOptIn
+        style={{
+          backgroundColor: "var(--main-bg-color)",
+          borderRadius: "16px",
+          padding: "8px 16px",
+        }}
+      />
+
+      <p>
+        Errors can happen for various reasons, including when an OTT website
+        updates their design.
+      </p>
+
+      <p className="data-collection-details">
+        Alongside the actual error details, the following information will be
+        collected to help reproduce the error:
+        <ul>
+          <li>Device, OS, and browser</li>
+          <li>Width and height of the browser</li>
+          <li>URL of webpage where the error occurred</li>
+        </ul>
+      </p>
+
+      <p>
+        Opting-in helps the developer detect and fix issues faster, so please
+        consider it.
+      </p>
+    </div>
+  );
 }
