a7c0495744da1fd3c92bc8da3be60b3a11274d64
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-03-23 14:38:40 +0530

    First page of onboarding flow now has slightly different text for returning users

--- extension/popup/OnboardingFlow/WelcomePage.tsx
+++ extension/popup/OnboardingFlow/WelcomePage.tsx
@@ -1,5 +1,11 @@
-import { type Dispatch, type StateUpdater } from "preact/hooks";
+import {
+  useEffect,
+  useState,
+  type Dispatch,
+  type StateUpdater,
+} from "preact/hooks";
 import { supportedSites, type Sitename } from "../common";
+import { getSetting } from "../../common";
 import SiteChooserFormControl from "../SiteChooserFormControl";
 
 interface WelcomePageProps {
@@ -11,11 +17,24 @@ export default function WelcomePage({
   selectedSites,
   setSelectedSites,
 }: WelcomePageProps) {
+  const [isExistingUser, setIsExistingUser] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const popupSeen = await getSetting("popupSeenAtLeastOnce");
+      if (typeof popupSeen === "boolean") setIsExistingUser(true);
+    })();
+  }, []);
+
   return (
     <div className="page welcome-page">
       <p>Thank you for choosing Sift!</p>
 
-      <p>This extension can add ratings to a number of OTT websites.</p>
+      {isExistingUser ? (
+        <p>You can now select exactly which OTT websites Sift operates on.</p>
+      ) : (
+        <p>This extension can add ratings to a number of OTT websites.</p>
+      )}
 
       <div>
         <h4 style={{ marginBottom: "4px" }}>
