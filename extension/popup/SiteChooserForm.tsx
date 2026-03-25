1334e89ef7566604d15f483c4bc01fbcdd81f85e
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-03-25 16:10:36 +0530

    Refactored SiteChooserForm so the state vars deal with Sitenames rather than PermStrings

--- extension/popup/SiteChooserForm.tsx
+++ extension/popup/SiteChooserForm.tsx
@@ -18,7 +18,7 @@ import SiteChooserFormControl from "./SiteChooserFormControl";
 import "./SiteChooserForm.css";
 
 type IsEnabled = boolean;
-const msDelayBeforeRequestingOrRenouncingPerms = 2000;
+const msDelayBeforeEnablingSites = 2000;
 
 export default function SiteChooserForm() {
   const [error, setError] = useState<Error | null>(null);
@@ -39,10 +39,10 @@ export default function SiteChooserForm() {
   //   popup
   // So the UX for a user that wants to enable multiple sites
   //   in quick succession would be absolutely horrible
-  const [pendingPerms, setPendingPerms] = useState<PermString[]>([]);
+  const [sitesToEnable, setSitesToEnable] = useState<Sitename[]>([]);
   const timeoutRef = useRef<number | null>(null);
 
-  // update sitePerms state based on perms that have already been granted
+  // update state based on perms that have already been granted
   useEffect(() => {
     (async () => {
       const previouslyGrantedHostPerms =
@@ -59,107 +59,95 @@ export default function SiteChooserForm() {
   //   has stopped interacting with the form
   useEffect(() => {
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
-    if (pendingPerms.length === 0) return;
-    timeoutRef.current = setTimeout(
-      requestPendingPerms,
-      msDelayBeforeRequestingOrRenouncingPerms,
-    );
+    if (sitesToEnable.length === 0) return;
+    timeoutRef.current = setTimeout(enableSites, msDelayBeforeEnablingSites);
 
-    async function requestPendingPerms() {
+    async function enableSites() {
       try {
         const granted = await browser.permissions.request({
-          origins: pendingPerms,
+          origins: sitesToEnable.flatMap((s) => supportedSites[s].permStrings),
         });
         if (!granted) throw new Error(ErrorMessage.hostPermissionNotGranted);
       } catch (e) {
         ensureError(e);
 
-        // reverse optimistic update to sitePerms
-        setEnabledSites((sp) => ({
-          ...sp,
-          ...pendingPerms
-            .map((ps) => permStringToSitename[ps])
-            .reduce((acc, s) => Object.assign(acc, { [s]: false }), {}),
+        // reverse optimistic update in toggleSite
+        setEnabledSites((prev) => ({
+          ...prev,
+          ...sitesToEnable.reduce(
+            (acc, s) => Object.assign(acc, { [s]: false }),
+            {},
+          ),
         }));
 
         if (e.message !== ErrorMessage.hostPermissionNotGranted) {
           handlePermissionError(e);
         }
       } finally {
-        setPendingPerms([]);
+        setSitesToEnable([]);
       }
     }
-  }, [pendingPerms]);
+  }, [sitesToEnable]);
 
   return (
     <form className="site-chooser-form">
       <h4>What sites should Sift run on?</h4>
       {error ? <p className="error">Sorry, something went wrong ...</p> : null}
 
-      {(Object.keys(supportedSites) as Sitename[]).map((site) => {
-        const { permStrings: pss } = supportedSites[site];
-        return (
-          <SiteChooserFormControl
-            key={site}
-            site={site}
-            enabled={enabledSites[site]}
-            loading={pss.some((ps) => pendingPerms.includes(ps))}
-            onToggle={toggleSitePerms}
-          />
-        );
-      })}
+      {(Object.keys(supportedSites) as Sitename[]).map((site) => (
+        <SiteChooserFormControl
+          key={site}
+          site={site}
+          enabled={enabledSites[site]}
+          loading={sitesToEnable.includes(site)}
+          onToggle={toggleSite}
+        />
+      ))}
     </form>
   );
 
-  async function toggleSitePerms(site: Sitename) {
+  async function toggleSite(site: Sitename) {
     const isEnabled = enabledSites[site];
 
     // optimistic update
     setEnabledSites({ ...enabledSites, [site]: !isEnabled });
 
-    // being careful not to mutate the supportedSites obj
-    let permStrings = supportedSites[site].permStrings.concat() as PermString[];
-
     if (!isEnabled) {
-      // User wants to enable site
-      setPendingPerms((pps) => pps.concat(permStrings));
+      /* User wants to enable site */
+      setSitesToEnable(sitesToEnable.concat(site));
       return;
     }
 
-    // User wants to disable site
+    /* User wants to disable site */
 
-    // Due to optimistic update (see above) when granting perms, we
-    //   may be in a situation where the user is trying to revoke a
-    //   perm that has not yet actually been granted
-    // If we detect that we're in this edge-timeline, all we should
-    //   do is remove the perm from pendingPerms
-    if (permStrings.some((ps) => pendingPerms.includes(ps))) {
-      setPendingPerms((pps) => pps.filter((pp) => !permStrings.includes(pp)));
-      permStrings = permStrings.filter((ps) => !pendingPerms.includes(ps));
+    // The user may enable Sift for a site by accident, then quickly
+    //   correct that mistake by disabling it before we've had time
+    //   to request the necessary permissions
+    // In this situation, all that needs to be done is to remove the
+    //   site in question from sitesToEnable
+    if (sitesToEnable.includes(site)) {
+      setSitesToEnable(sitesToEnable.filter((s) => s !== site));
+      return;
     }
 
-    if (permStrings.length > 0) {
-      // The service worker will take care of removing sift from any
-      //   already-open webpages associated with the perms we're about
-      //   to remove
-      // The code to revoke the permission could have been called from
-      //   here as well, but since we want a bit of a delay between the
-      //   user disabling Sift for a site, and the permission-revoke API
-      //   call (so content-scripts have time to receive and react to the
-      //   clean up order), there was a risk that the user would close the
-      //   popup before the delay ended and the permission revoked
-      const response = await browser.runtime.sendMessage<
-        Message,
-        SWMessageResponse<unknown>
-      >({
-        type: MessageType.sitesDisabled,
-        data: { sites: [site] },
-      } satisfies Message);
-      if ("error" in response) {
-        // reverse the optimistic update
-        setEnabledSites({ ...enabledSites, [site]: isEnabled });
-        return;
-      }
+    // The service worker will take care of removing sift from any
+    //   already-open webpages associated with the perms we're about
+    //   to remove
+    // The code to revoke the permission could have been called from
+    //   here as well, but since we want a bit of a delay between the
+    //   user disabling Sift for a site, and the permission-revoke API
+    //   call (so content-scripts have time to receive and react to the
+    //   clean up order), there was a risk that the user would close the
+    //   popup before the delay ended, which means we'd never actually
+    //   get around to renouncing the permission
+    const response = await browser.runtime.sendMessage<
+      Message,
+      SWMessageResponse<unknown>
+    >({ type: MessageType.sitesDisabled, data: { sites: [site] } });
+    if ("error" in response) {
+      // reverse the optimistic update
+      setEnabledSites({ ...enabledSites, [site]: isEnabled });
+      return;
     }
   }
 
