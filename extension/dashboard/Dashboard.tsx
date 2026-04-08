61cb8b6c8c18425c4bd98048b95bce7f42dc3ed9
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-04-08 13:00:41 +0530

    Dashboard component is now able to pull data out of TelemetryStore

--- extension/dashboard/Dashboard.tsx
+++ extension/dashboard/Dashboard.tsx
@@ -1,16 +1,54 @@
 import { useEffect, useState } from "preact/hooks";
 import TelemetryStore from "../common/TelemetryStore";
+import type { ErrorDetails, WebpageStats } from "../common/types";
 
 interface DashboardProps {
   telemetryStore: TelemetryStore;
 }
 
-export default function Dashboard({ telemetryStore }: DashboardProps) {
-  const [stats, setStats] = useState({});
+interface Stats {
+  nProgramRatingRequests: { timestamp: number; value: number }[];
+  nRatingsApiRequests: { timestamp: number; value: number }[];
+  ratingsApiResponseTimes: { timestamp: number; value: number[] }[];
+  webpageRatingStats: { timestamp: number; value: WebpageStats }[];
+  errors: { timestamp: number; value: ErrorDetails[] }[];
+}
 
+export default function Dashboard({ telemetryStore }: DashboardProps) {
+  const [stats, setStats] = useState<Stats | null>(null);
+
+  // pull data out of the telemetry store
   useEffect(() => {
-    // pull data out of the telemetry store
+    (async () => {
+      const [
+        nProgramRatingRequests,
+        nRatingsApiRequests,
+        ratingsApiResponseTimes,
+        webpageRatingStats,
+        errors,
+      ] = await Promise.all([
+        telemetryStore.getRecords<number>("PROGRAM_RATING_REQUEST_RECEIVED"),
+        telemetryStore.getRecords<number>("RATINGS_API_REQUEST_MADE"),
+        telemetryStore.getRecords<number[]>("RATINGS_API_RESPONSE_RECEIVED"),
+        telemetryStore.getRecords<WebpageStats>(
+          "WEBPAGE_RATING_STATS_RECEIVED",
+        ),
+        telemetryStore.getRecords<ErrorDetails[]>("ERROR"),
+      ]);
+      setStats({
+        nProgramRatingRequests,
+        nRatingsApiRequests,
+        ratingsApiResponseTimes,
+        webpageRatingStats,
+        errors,
+      });
+    })();
   }, []);
 
-  return <div class="dashboard"></div>;
+  if (!stats) return null;
+
+  return (
+    <div class="dashboard">
+    </div>
+  );
 }
