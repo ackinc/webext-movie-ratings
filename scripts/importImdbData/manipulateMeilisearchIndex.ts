bd4eb07be78f50e96743c60411851f213162d1c6
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-04-20 23:36:29 +0530

    Better implementation of manipMeilisearch script functionality using yargs commands

--- scripts/importImdbData/manipulateMeilisearchIndex.ts
+++ scripts/importImdbData/manipulateMeilisearchIndex.ts
@@ -5,45 +5,40 @@ import { hideBin } from "yargs/helpers";
 
 const { MEILISEARCH_MASTER_KEY, MEILISEARCH_URL } = process.env;
 
-const argv = yargs(hideBin(process.argv))
-  .option("info", {
-    boolean: true,
-    default: false,
-  })
-  .option("stats", {
-    boolean: true,
-    default: false,
-  })
-  .option("list", {
-    number: true,
-  })
-  .option("search", {
-    string: true,
-  })
-  .option("clear", {
-    boolean: true,
-    default: false,
-  })
-  .option("delete", {
-    boolean: true,
-    default: false,
-  })
-  .parseSync();
-
 const client = new Meilisearch({
   host: MEILISEARCH_URL!,
   apiKey: MEILISEARCH_MASTER_KEY!,
 });
 const index = client.index<Document>("imdb");
 
-if (argv.info || argv.stats) {
-  console.log(await index.getRawInfo(), await index.getStats());
-} else if (argv.list) {
-  console.log(await index.getDocuments({ limit: argv.list }));
-} else if (argv.search) {
-  console.log(await index.search(argv.search));
-} else if (argv.clear) {
-  await index.deleteAllDocuments();
-} else if (argv.delete) {
-  await index.delete();
-}
+await yargs(hideBin(process.argv))
+  .command(
+    ["$0", "info", "stats"],
+    "get basic info about the index",
+    {},
+    async () => console.log(await index.getRawInfo(), await index.getStats()),
+  )
+  .command(
+    ["list"],
+    "list documents in index",
+    { limit: { number: true, default: 10 } },
+    async (argv) =>
+      console.log(await index.getDocuments({ limit: argv.limit! })),
+  )
+  .command(
+    ["search"],
+    "search for documents matching the query",
+    {
+      query: { string: true, demandOption: true },
+      limit: { number: true, default: 10 },
+    },
+    async (argv) =>
+      console.log(await index.search(argv.query, { limit: argv.limit })),
+  )
+  .command(["clear"], "remove all documents from the index", {}, async () => {
+    await index.deleteAllDocuments();
+  })
+  .command(["delete"], "delete the index", {}, async () => {
+    await index.delete();
+  })
+  .parse();
