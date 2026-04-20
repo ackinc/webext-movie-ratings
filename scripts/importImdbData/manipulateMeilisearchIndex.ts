1198a80be9a5e69c5908d0f7c3c35e56889a8582
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-04-20 17:49:04 +0530

    Minor: added functionality to manipulate Meilisearch index script

---
+++ scripts/importImdbData/manipulateMeilisearchIndex.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import { Meilisearch } from "meilisearch";
+
+const { MEILISEARCH_MASTER_KEY, MEILISEARCH_URL } = process.env;
+const argv = process.argv.slice(2);
+
+const client = new Meilisearch({
+  host: MEILISEARCH_URL!,
+  apiKey: MEILISEARCH_MASTER_KEY!,
+});
+const index = client.index<Document>("imdb");
+
+if (argv.length === 0 || argv.includes("--stats")) {
+  console.log(await index.getStats());
+  process.exit(0);
+}
+
+if (argv.some((arg) => arg.startsWith("--search"))) {
+  const searchTerm = argv
+    .find((arg) => arg.startsWith("--search"))
+    ?.split("=")[1];
+  console.log(await index.search(searchTerm));
+  process.exit(0);
+}
+
+if (argv.includes("--clear")) {
+  await index.deleteAllDocuments();
+  process.exit(0);
+}
+
+if (argv.includes("--delete")) {
+  await index.delete();
+  process.exit(0);
+}
