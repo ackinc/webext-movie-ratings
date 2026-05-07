9ef33e32d687be559b8a376c9726c35f8547bd67
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-05-07 14:04:04 +0530

    Now capturing errors in refreshImdbData process in Sentry

--- packages/api-server/scripts/refreshImdbData.ts
+++ packages/api-server/scripts/refreshImdbData.ts
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
+import "../instrument.ts";
+
 import { execFileSync } from "node:child_process";
 import * as path from "node:path";
 import { fileURLToPath } from "node:url";
+import { pick } from "siftutils";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const { IMDB_DATA_DIR } = process.env;
-
-if (!IMDB_DATA_DIR) throw new Error(`IMDB_DATA_DIR not in env`);
+const { IMDB_DATA_DIR } = pick(process.env, ["IMDB_DATA_DIR"], true);
 
 execFileSync(
   path.join(__dirname, "downloadImdbData.ts"),
