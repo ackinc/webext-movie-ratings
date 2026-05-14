9ca7324eb167c113fb613e68855058d549aa29b9
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-05-14 19:37:21 +0530

    Added script to pull prod data (from latest db backup) to local; this will help with analyzing the effectiveness of our program-matching

---
+++ packages/api-server/scripts/pullProdDataFromBackup.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# This script will be run as a cron-job on my local machine at the top
+#   of every hour to maximize the chances of it running at least once a
+#   day (it won't run if the laptop is not on!)
+# Because of this, it's important to have early-detection for whether
+#   it has already run today 
+
+set -eu
+
+set -a
+source "${ENVFILE:-.env}"
+set +a
+
+# function definitions
+
+assert_env() {
+  local var_name="$1"
+  if [[ -z "${!var_name:-}" ]]; then
+    echo "Error: env var $var_name is not set" >&2
+    exit 1
+  fi
+}
+
+restore_table() {
+    local from_db="$1"
+    local to_db="$2"
+    local table_name="$3"
+
+    # convert to file containing sql statements
+    sqlite3 "$from_db" ".dump $table_name" |
+        # replace table name into which data will be inserted so it doesn't
+        #   clash with data already in dest DB
+        sed "s/CREATE TABLE IF NOT EXISTS \"$table_name\"/DROP TABLE IF EXISTS \"prod_$table_name\";\nCREATE TABLE IF NOT EXISTS \"prod_$table_name\"/g" |
+        sed "s/INSERT INTO $table_name/INSERT INTO prod_$table_name/g" |
+        sqlite3 "$to_db"
+    
+    echo "Restored $table_name into prod_$table_name"
+}
+
+# start
+
+assert_env DB_BACKUP_URI
+assert_env DB_PATH
+
+DB_BACKUP_PATH="$(dirname $DB_PATH)"/db-prod-bak.sqlite
+DB_BACKUP_DATE=$( stat -c %y $DB_BACKUP_PATH | cut -d" " -f1 )
+RUN_CHECKS=$([[ " $@ " == *" --skip-checks "* ]] && echo 0 || echo 1)
+TABLES_TO_RESTORE=("titles")
+TODAY=$( date --iso-8601 )
+
+if [[ $DB_BACKUP_DATE < $TODAY ]]; then
+    aws s3 cp "$DB_BACKUP_URI" "$DB_BACKUP_PATH"
+elif [[ $RUN_CHECKS -eq 1 ]]; then
+    echo "This script already ran today. Exiting early ..."
+    exit 0
+fi;
+
+for table_name in "${TABLES_TO_RESTORE[@]}"; do
+    restore_table "$DB_BACKUP_PATH" "$DB_PATH" "$table_name"
+done
