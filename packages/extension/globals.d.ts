9d8cbf3ece911ab9f025b8c3876e996f506f2ffa
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-05-25 15:30:34 +0530

    Extension now opens uninstall-reason page on uninstall

--- packages/extension/globals.d.ts
+++ packages/extension/globals.d.ts
@@ -6,6 +6,7 @@ declare const APP_ENV: "development" | "testing" | "production";
 declare const TARGET_BROWSER: "chrome" | "firefox" | "edge";
 
 declare const SIFT_API_URL: string;
+declare const SIFT_WEBSITE_URL: string;
 
 declare const OMDB_API_KEY: string;
 declare const ISOLATED_CONTENT_SCRIPT_PATH: string;
