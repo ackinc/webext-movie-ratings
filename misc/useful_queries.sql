bbe8a125bb681f6f83e6fd98cbcdfede420cf8db
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-05-25 12:59:38 +0530

    Minor: added a couple queries to the useful-queries file

--- misc/useful_queries.sql
+++ misc/useful_queries.sql
@@ -3,3 +3,13 @@ SELECT
 	imdbTitles.id, imdbTitles.title, imdbTitles.type, imdbTitles.year
 FROM titles
 LEFT JOIN imdbTitles ON titles.imdbId = imdbTitles.id;
+
+SELECT meta ->> 'originallyRequestedFrom' src, COUNT(*) n
+FROM titles
+GROUP BY src
+ORDER BY n DESC;
+
+SELECT status, COUNT(*) n
+FROM titles
+GROUP BY status
+ORDER BY n DESC;
