d49c00163bc6dc276103c4535f0eb89d3bfc29b4
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-05-26 16:53:00 +0530

    Minor: extracted IconProps defn

--- packages/extension/popup/components/Icons/ExclmarkOnCircle.tsx
+++ packages/extension/popup/components/Icons/ExclmarkOnCircle.tsx
@@ -1,10 +1,12 @@
-export default function ExclmarkOnCircle({ style }: { style?: object }) {
+import type { IconProps } from "./types";
+
+export default function ExclmarkOnCircle(props: IconProps) {
   return (
     <svg
       viewBox="0 0 16 16"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
-      style={{ ...style }}
+      {...props}
     >
       <path d="M8 0c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zM9 13h-2v-2h2v2zM9 10h-2v-7h2v7z"></path>
     </svg>
