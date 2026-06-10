93cf739156159bc06df22f0c9a8c2eb396b66719
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-06-10 14:01:46 +0530

    ratingsService now tracks operations-in-flight and avoids making unnecessary api requests

--- packages/extension/service-worker/ratingsService.ts
+++ packages/extension/service-worker/ratingsService.ts
@@ -1,5 +1,4 @@
 import { addWeeks } from "date-fns";
-import { type SiftApiProgramMatching } from "sifttypes";
 import { type IMDBData, type ProgramData } from "@common";
 import TelemetryStore from "@common/TelemetryStore";
 import * as siftApiService from "@common/siftApiService";
@@ -7,47 +6,15 @@ import OmdbApiClient from "./OmdbApiClient";
 
 let telemetryStore: TelemetryStore;
 let omdbApiClient: OmdbApiClient;
+let inProgress: Record<
+  string,
+  { resolve: (x: ImdbDataFetchResult) => void; reject: (e: Error) => void }[]
+>;
 
 export function initialize(teleStore: TelemetryStore) {
   omdbApiClient = new OmdbApiClient(fetchWithAddedTelemetry);
   telemetryStore = teleStore;
-}
-
-interface ImdbDataFetchResult {
-  imdbData: IMDBData;
-  expiry: Date;
-}
-export async function fetchIMDBData(
-  imdbIdOrProgram: string | ProgramData,
-  pageUrl: string,
-): Promise<ImdbDataFetchResult> {
-  let imdbData = await omdbApiClient.fetchIMDBData(imdbIdOrProgram);
-  // represents whether we have the imdbId for the program
-  let matchStatus: SiftApiProgramMatching.Response["status"] | undefined =
-    undefined;
-
-  if (typeof imdbIdOrProgram === "string" || imdbData.imdbID) {
-    matchStatus = "matched";
-  } else {
-    // we didn't have the program's imdb id, and the omdb api wasn't
-    //   able to figure it out based on the program's details; let's see
-    //   if sift's program-matching can do it
-    let matchedImdbId;
-    ({ status: matchStatus, imdbId: matchedImdbId } =
-      await siftApiService.getMatchedImdbId(
-        imdbIdOrProgram as ProgramData,
-        pageUrl,
-      ));
-
-    if (matchedImdbId) {
-      // try to get the imdb data from omdb by querying with the
-      //   imdb id we just matched this program to
-      imdbData = await omdbApiClient.fetchIMDBData(matchedImdbId);
-    }
-  }
-
-  const expiry = addWeeks(new Date(), imdbData.imdbRating === "N/F" ? 1 : 2);
-  return { imdbData, expiry };
+  inProgress = {};
 }
 
 async function fetchWithAddedTelemetry(
@@ -74,4 +41,67 @@ async function fetchWithAddedTelemetry(
   }
 
   return response;
+}
+
+interface ImdbDataFetchResult {
+  imdbData: IMDBData;
+  expiry: Date;
+}
+export async function fetchIMDBData(
+  imdbIdOrProgram: string | ProgramData,
+  pageUrl: string,
+): Promise<ImdbDataFetchResult> {
+  return new Promise<ImdbDataFetchResult>((resolve, reject) => {
+    const key = getKey();
+    if (key in inProgress) {
+      inProgress[key]!.push({ resolve, reject });
+    } else {
+      inProgress[key] = [{ resolve, reject }];
+      helper(key);
+    }
+  });
+
+  async function helper(key: string) {
+    let result: ImdbDataFetchResult;
+    let error: Error;
+    try {
+      let imdbData = await omdbApiClient.fetchIMDBData(imdbIdOrProgram);
+
+      if (!(typeof imdbIdOrProgram === "string" || imdbData.imdbID)) {
+        // we didn't have the program's imdb id, and the omdb api wasn't
+        //   able to figure it out based on the program's details; let's see
+        //   if sift's program-matching can do it
+        const { imdbId: matchedImdbId } = await siftApiService.getMatchedImdbId(
+          imdbIdOrProgram as ProgramData,
+          pageUrl,
+        );
+
+        if (matchedImdbId) {
+          // try to get the imdb data from omdb by querying with the
+          //   imdb id we just matched this program to
+          imdbData = await omdbApiClient.fetchIMDBData(matchedImdbId);
+        }
+      }
+
+      result = {
+        imdbData,
+        expiry: addWeeks(new Date(), imdbData.imdbRating === "N/F" ? 1 : 2),
+      };
+    } catch (e) {
+      error =
+        e instanceof Error
+          ? e
+          : new Error("Error fetching imdb data", { cause: e });
+    } finally {
+      inProgress[key]!.forEach(({ resolve, reject }) =>
+        error ? reject(error) : resolve(result),
+      );
+      delete inProgress[key];
+    }
+  }
+
+  function getKey() {
+    if (typeof imdbIdOrProgram === "string") return imdbIdOrProgram;
+    return JSON.stringify(imdbIdOrProgram);
+  }
 }
