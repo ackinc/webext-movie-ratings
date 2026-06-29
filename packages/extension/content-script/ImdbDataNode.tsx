19b41e2026143147385ce238f71211f275242d90
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-06-29 13:51:30 +0530

    Bugfix: imdb node styles weren't being applied

--- packages/extension/content-script/ImdbDataNode.tsx
+++ packages/extension/content-script/ImdbDataNode.tsx
@@ -1,4 +1,3 @@
-import { useState } from "preact/hooks";
 import {
   browser,
   CssClasses,
@@ -18,49 +17,32 @@ export default function ImdbDataNode({
   program,
   imdbData: data,
 }: ImdbDataNodeProps) {
-  const [expanded, setExpanded] = useState(false);
-
-  const showNode = data.imdbRating !== "N/F";
-
   return (
-    <div
-      className={CssClasses.imdbDataNodeContent}
-      onMouseEnter={() => showNode && setExpanded(true)}
-      onMouseLeave={() => showNode && setExpanded(false)}
-      style={{
-        display: "flex",
-        gap: "16px",
-        visibility: showNode ? "visibile" : "hidden",
-      }}
-    >
-      <a
-        href={getIMDBLink(data.imdbId)}
-        target="_blank"
-        onClick={(e) => e.stopPropagation()}
-      >
-        IMDb{" "}
-        {typeof data.imdbRating === "number"
-          ? data.imdbRating.toFixed(1)
-          : data.imdbRating}
-      </a>
-      <button
-        onClick={() =>
-          browser.runtime.sendMessage({
-            type: MessageType.reportIncorrectProgramMatch,
-            data: { program, imdbData: data, pageUrl: location.href },
-          } satisfies Message)
-        }
-        style={{
-          margin: 0,
-          border: 0,
-          padding: 0,
-          backgroundColor: "transparent",
-          color: "white",
-          visibility: expanded ? "visible" : "hidden",
-        }}
-      >
-        Wrong?
-      </button>
+    <div className={CssClasses.imdbDataNodeContent}>
+      <div className="headline">
+        <a
+          className="rating-page-link"
+          href={getIMDBLink(data.imdbId)}
+          target="_blank"
+          onClick={(e) => e.stopPropagation()}
+        >
+          IMDb{" "}
+          {typeof data.imdbRating === "number"
+            ? data.imdbRating.toFixed(1)
+            : data.imdbRating}
+        </a>
+        <button
+          className="maybe-wrong-button"
+          onClick={() =>
+            browser.runtime.sendMessage({
+              type: MessageType.reportIncorrectProgramMatch,
+              data: { program, imdbData: data, pageUrl: location.href },
+            } satisfies Message)
+          }
+        >
+          Wrong?
+        </button>
+      </div>
     </div>
   );
 }
