3990762eeeb5ed11a84a972027b62390794d38d0
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2026-09-25 15:18:39 +0530

    We now run migrations on every deploy

--- packages/api-server/scripts/aws/deploy.sh
+++ packages/api-server/scripts/aws/deploy.sh
@@ -7,17 +7,20 @@ cd ~/sift
 HEAD_OLD=`cat .git/refs/heads/master`
 
 git pull
-# --ignore-scripts flag prevents the prepare script in the project's
-#   root package.json from running, which would fail because husky
-#   is not being installed here
+# The --ignore-scripts flag in the commands below prevents the prepare script
+#   in the project's root package.json from running; we want this because that
+#   script would fail anyway, because it invokes husky which is not even
+#   installed in this environment
 pnpm --filter "./shared/**" install --ignore-scripts --prod
 pnpm --filter "./packages/api-server" install --ignore-scripts --prod
 
 cd ~/sift/packages/api-server
 aws ssm get-parameter --name "/sift/env.production" --query "Parameter.Value" --output text > .env
 
-X=$(git diff --name-only $HEAD_OLD HEAD | grep packages/api-server/ecosystem.config.cjs | wc -l)
-if [ $X -gt 0 ]; then
+pnpm run migrations
+
+PM2_CONFIG_CHANGED=$(git diff --name-only $HEAD_OLD HEAD | grep packages/api-server/ecosystem.config.cjs | wc -l)
+if [ $PM2_CONFIG_CHANGED -gt 0 ]; then
     # pm2 config was changed
     pm2 delete all
     pm2 start ecosystem.config.cjs
